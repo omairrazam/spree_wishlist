cc4fcc4b4c7f5381eaffa4aae5bd6ead68e31c1d
Shahbaz Hassan <yaseeniqbal119@gmail.com>
2019-03-13 15:30:01 +0500

    Add Alphabatical Scope for Wished Products

--- app/models/spree/wished_product.rb
+++ app/models/spree/wished_product.rb
@@ -2,6 +2,8 @@ class Spree::WishedProduct < ActiveRecord::Base
   belongs_to :variant
   belongs_to :wishlist
 
+  scope :sort_alphabatical_by_name, -> {includes(variant: {product: :translations}).order("spree_product_translations.name ASC")}
+
   def total
     quantity * variant.price
   end
